6f685b4668bec91d51fd779deb1ac3765423ee90
Johannes Isphording <mail@johannesisphording.de>
2023-10-03 18:43:01 +0200

    [update] added a sphere to the scene

--- RayTracingWeekend/src/Rendering.cpp
+++ RayTracingWeekend/src/Rendering.cpp
@@ -21,7 +21,22 @@ void rendering::message(std::string message)
 // U T I L I T Y
 // ---------- ---------- ---------- ---------- ---------- //
 
+// Hit Sphere
+bool hit_sphere(const point3& center, double radius, const Ray& ray) {
+	vec3 oc = ray.origin() - center;
+	auto a = dot(ray.direction(), ray.direction());
+	auto b = 2.0 * dot(oc, ray.direction());
+	auto c = dot(oc, oc) - radius * radius;
+	auto discriminant = b * b - 4 * a * c;
+	return (discriminant >= 0);
+}
+
+// Ray color
 color::color ray_color(const Ray& m_ray) {
+	// Color Red if sphere is hit
+	if (hit_sphere(point3(0, 0, -1), 0.5, m_ray))
+		return color::color(1, 0, 0);
+
 	vec3 unit_direction = unit_vector( m_ray.direction());
 	auto a = 0.5 * (unit_direction.y() + 1.0);
 	
