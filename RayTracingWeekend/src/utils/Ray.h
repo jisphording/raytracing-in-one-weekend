343451c66a1970ef3563ead530cb12dc07eed3ff
Johannes Isphording <mail@johannesisphording.de>
2023-10-03 15:38:16 +0200

    [init] added a Ray class

---
+++ RayTracingWeekend/src/utils/Ray.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "./vec3.h"
+
+class Ray
+{
+public:
+	Ray() {}
+
+	Ray(const point3& origin, const vec3& direction) :
+		m_origin(origin),
+		m_direction(direction)
+	{}
+
+	point3 origin() const { return m_origin; }
+	vec3 direction() const { return m_direction; }
+
+	point3 at(double t) const
+	{
+		return m_origin + t * m_direction;
+	}
+
+private:
+	point3 m_origin;
+	vec3 m_direction;
+};
